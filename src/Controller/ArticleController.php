53b9daea738975691835aeaacb538a150d2797a1
Gersey Stelmach <gerseystelmach@gmail.com>
2021-07-22 00:10:25 +0200

    US10_Blog

---
+++ src/Controller/ArticleController.php
@@ -0,0 +1,102 @@
+<?php
+
+namespace App\Controller;
+
+use DateTime;
+use App\Entity\Article;
+use App\Form\ArticleType;
+use App\Repository\ArticleRepository;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+/**
+ * @Route("admin/article")
+ */
+class ArticleController extends AbstractController
+{
+    /**
+     * @Route("/", name="article_index", methods={"GET"})
+     * @IsGranted("IS_AUTHENTICATED_FULLY")  
+     */
+    public function index(ArticleRepository $articleRepository): Response
+    {
+        return $this->render('article/index.html.twig', [
+            'articles' => $articleRepository->findAll(),
+        ]);
+    }
+
+    /**
+     * @Route("/new", name="article_new", methods={"GET","POST"})
+     * @IsGranted("IS_AUTHENTICATED_FULLY") 
+     */
+    public function new(Request $request): Response
+    {
+        $article = new Article();
+        $form = $this->createForm(ArticleType::class, $article);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $article->setCreatedAt(new DateTime('now'));
+            $entityManager->persist($article);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('article_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('article/new.html.twig', [
+            'article' => $article,
+            'form' => $form,
+        ]);
+    }
+
+    /**
+     * @Route("/{id}", name="article_show", methods={"GET"})
+     * @IsGranted("IS_AUTHENTICATED_FULLY") 
+     */
+    public function show(Article $article): Response
+    {
+        return $this->render('article/show.html.twig', [
+            'article' => $article,
+        ]);
+    }
+
+    /**
+     * @Route("/{id}/edit", name="article_edit", methods={"GET","POST"})
+     * @IsGranted("IS_AUTHENTICATED_FULLY") 
+     */
+    public function edit(Request $request, Article $article): Response
+    {
+        $form = $this->createForm(ArticleType::class, $article);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->getDoctrine()->getManager()->flush();
+
+            return $this->redirectToRoute('article_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('article/edit.html.twig', [
+            'article' => $article,
+            'form' => $form,
+        ]);
+    }
+
+    /**
+     * @Route("/{id}", name="article_delete", methods={"POST"})
+     * @IsGranted("IS_AUTHENTICATED_FULLY") * 
+     */
+    public function delete(Request $request, Article $article): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$article->getId(), $request->request->get('_token'))) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->remove($article);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('article_index', [], Response::HTTP_SEE_OTHER);
+    }
+}
